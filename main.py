2d9b532161134871609ea1fa852e638fbfcb5652
andylushman <andylushman@gmail.com>
2022-04-18 18:13:34 +0200

    Request Body

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 class ModelName(str, Enum):
@@ -13,7 +14,10 @@ class ModelName(str, Enum):
 app = FastAPI()
 
 
+#################
 # Path Parameters
+#################
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
@@ -25,7 +29,10 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
+#################
 # Query Parameters
+#################
+
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
@@ -36,7 +43,7 @@ async def read_item(skip: int = 0, limit: int = 10):
 
 @app.get("/items/{item_id}")
 async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+        item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
 ):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
@@ -54,3 +61,35 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+#################
+# REQUEST BODY
+#################
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+# The function parameters will be recognized as follows:
+# If the parameter is also declared in the path, it will be used as a path parameter.
+# If the parameter is of a singular type (like int, float, str, bool, etc) it will be interpreted as a query parameter.
+# If the parameter is declared to be of the type of a Pydantic model, it will be interpreted as a request body.
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
